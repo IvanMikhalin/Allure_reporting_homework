83c16016c124dcdd06158f156314964d5f79c516
Ivan Mikhalin <mikhalin_ivan@mail.ru>
2022-10-26 12:11:46 +0400

    tests amended, packages transfered

---
+++ src/test/java/ru/netology/delivery/test/data/DateGenerator.java
@@ -0,0 +1,15 @@
+package ru.netology.delivery.test.data;
+
+public class DateGenerator {
+
+    public DateGenerator(String date) {
+        this.date = date;
+    }
+
+    public String getData() {
+        return date;
+    }
+
+    private final String date;
+
+}
